5aa7dde3d5e5f4bcb2df04c2c3c6495919bddf62
jagjeet-singh1812 <110558458+jagjeet-singh1812@users.noreply.github.com>
2022-08-10 13:34:23 +0530

    completed array part
    finally done with array !!!

--- DSA/ARRAY/Operation_in_array.c
+++ DSA/ARRAY/Operation_in_array.c
@@ -18,21 +18,87 @@ void insertion(int *arr[1000], int n, int size, int index, int element)
     }
 }
 
-
-void deletion(int *arr[1000],int deletion_element_index,int n,int size)
+void deletion(int *arr[1000], int deletion_element_index, int n, int size)
 {
-    if(n>size)
+    if (n > size)
     {
         printf("THE DELETION CAN OCCUR PLS CHECK ARRAY MAX SIZE");
     }
-    else{
-    for (int i = deletion_element_index; i<n; i++)
+    else
     {
-        arr[i]=arr[i+1];
+        for (int i = deletion_element_index; i < n; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
     }
-    }
-    
 }
+
+int linearsearch(int arr[1000], int n, int search_element)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == search_element)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+// This is bubble sort
+void sort(int *arr[1000],int n)
+{ int temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+int binarysearch(int arr[1000],int n,int search_element)
+{
+    int mid,high,low;
+    low=0;
+    high= n-1;
+    while(low<=high)
+    {
+    mid=high+low/2;
+        if(arr[mid]==search_element)
+        {
+            return mid;
+        }
+
+        if(arr[mid]<search_element)
+        {
+            low=mid+1;
+        }
+    
+        /* A comment. */
+        // it can also be done like this
+        // if(arr[mid]>search_element)
+        // {
+        //     high=mid-1;
+        // }
+        // else
+        // {
+        // low=mid+1;
+        // }
+        else
+        {
+            high=mid-1;
+        }
+    }
+    return -1;
+
+}
+
 
 void input(int *arr[1000], int n)
 {
@@ -55,13 +121,16 @@ void show(int *arr[1000], int n)
 void main()
 {
     int array[1000], n;
-    int max_size = 1000, insertion_element, index, option,deletion_element;
+    int max_size = 1000, insertion_element, index, option, deletion_element;
     printf("Enter the number of elements you want:- \n");
     scanf("%d", &n);
+    // n can be founf out by n= sizeof(arr)/sizeof(int) for an given array
     input(&(array), n);
     show(&(array), n);
     printf("SELECTION 1 FOR INSERTION\n");
     printf("SELECTION 2 FOR DELETION\n");
+    printf("SELECTION 3 FOR SEARCHING\n");
+    //time complexity of liner is O(n) and for binary is O(log n)
     printf("SELECT YOUR DESIRED OPTION:- \n");
     scanf("%d", &option);
 
@@ -77,15 +146,65 @@ void main()
         n += 1;
         printf("Array after insertion is:-\n");
         show(&(array), n);
+        break;
     }
     case 2:
     {
-          printf("enter the index of the deletion element:-");
-          scanf("%d",&deletion_element);
-          deletion(&(array),deletion_element,n,max_size);
-          printf("Array after deleion is :-");
-          n-=1;
+        printf("enter the index of the deletion element:-");
+        scanf("%d", &deletion_element);
+        deletion(&(array), deletion_element, n, max_size);
+        printf("Array after deleion is :-");
+        n -= 1;
         show(&(array), n);
+        break;
+    }
+    case 3:
+    {
+        int option1;
+        printf("select 1 for linear search\n");
+        printf("select 2 for binary search\n");
+        // binary search time complexity is big O of log n(O(log n))
+        printf("select your choice :-\n");
+        scanf("%d", &option1);
+        switch (option1)
+        {
+            int search_element, req_index;
+        case 1:
+        {
+            printf("Enter the element you want to search");
+            scanf("%d", &search_element);
+            req_index = linearsearch(&(array), n, search_element);
+            if ((req_index) != -1)
+            {
+                printf("The Element %d was found at %d index of array\n", search_element, req_index);
+            }
+            else
+            {
+                printf("The Element %d is not found in array \n", search_element);
+            }
+            break;
+        }
+        case 2:
+        {
+            printf("first we need to sort\n");
+           sort(&(array),n);
+           printf("So array after sorting is:\n");
+           show(&(array),n);
+           printf("so now array is sorted \n");
+            printf("Enter the element you want to search:");
+            scanf("%d", &search_element);
+            req_index=binarysearch(array,n,search_element);
+            if ((req_index) != -1)
+            {
+                printf("The Element %d was found at %d index of array\n", search_element, req_index);
+            }
+            else
+            {
+                printf("The Element %d is not found in array \n", search_element);
+            }
+            break;
+        }
+        }
     }
     }
 }
